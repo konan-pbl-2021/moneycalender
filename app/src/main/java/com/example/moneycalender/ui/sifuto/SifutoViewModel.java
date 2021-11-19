b0ef95245486abd6d2d1a946280745a26971e322
s2071087 <s2071087@s.konan-u.ac.jp>
2021-11-19 15:03:20 +0900

    カレンダ＝を作成しました。

--- app/src/main/java/com/example/moneycalender/ui/sifuto/SifutoViewModel.java
+++ app/src/main/java/com/example/moneycalender/ui/sifuto/SifutoViewModel.java
@@ -1,16 +1,16 @@
-package com.example.moneycalender.ui.notifications;
+package com.example.moneycalender.ui.sifuto;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class SifutoViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public SifutoViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is home fragment");
     }
 
     public LiveData<String> getText() {
