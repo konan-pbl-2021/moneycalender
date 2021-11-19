a599f6d65d6b40625bad706f329b0b9f2a070026
bigflier <wsfkezhxbvp5996mneeb@docomo.ne.jp>
2021-11-19 14:17:57 +0900

    二日目最新版です

--- app/src/main/java/com/example/moneycalender/ui/sifuto/SifutoFragment.java
+++ app/src/main/java/com/example/moneycalender/ui/sifuto/SifutoFragment.java
@@ -4,29 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.moneycalender.R;
-import com.example.moneycalender.ui.home.HomeViewModel;
 
 public class SifutoFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private ShiftViewModel shiftViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-
-
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_sifuto, container, false);
+        ImageButton ButtonLeft = (ImageButton)root.findViewById(R.id.ButtonLeft);
+
+        shiftViewModel = ViewModelProviders.of(this).get(ShiftViewModel.class);
         return root;
     }
 }
