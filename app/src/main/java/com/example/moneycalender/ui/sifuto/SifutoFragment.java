5857f398b33187f6628aebb69b999b59ca9e9628
bigflier <wsfkezhxbvp5996mneeb@docomo.ne.jp>
2021-11-25 12:52:08 +0900

    持ち帰り作業しました

--- app/src/main/java/com/example/moneycalender/ui/sifuto/SifutoFragment.java
+++ app/src/main/java/com/example/moneycalender/ui/sifuto/SifutoFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -54,10 +55,29 @@ public class SifutoFragment extends Fragment {
         Sum.setText("合計収入"+ String.valueOf(sum) + "円");
 
         /*バイト名・時給*/
-        TextView workname = (TextView)root.findViewById(R.id.WorkName);
-        workname.setText("バイト名");
-        TextView workmoney = (TextView)root.findViewById(R.id.WorkMoney);
-        workmoney.setText("時給");
+        TextView Workname = (TextView)root.findViewById(R.id.WorkName);
+        Workname.setText("バイト名");
+        TextView Workmoney = (TextView)root.findViewById(R.id.WorkMoney);
+        Workmoney.setText("時給");
+
+        EditText workname1 = (EditText)root.findViewById(R.id.WorkName1);
+        String inpuStr_n1 = workname1.getText().toString();
+        EditText workmoney1 = (EditText)root.findViewById(R.id.WorkMoney1);
+        String inpuStr_m1 = workmoney1.getText().toString();
+        workmoney1.setText(String.valueOf(inpuStr_m1) + "円");
+
+        EditText workname2 = (EditText)root.findViewById(R.id.WorkName2);
+        String inpuStr_n2 = workname2.getText().toString();
+        EditText workmoney2 = (EditText)root.findViewById(R.id.WorkMoney2);
+        String inpuStr_m2 = workmoney2.getText().toString();
+        workmoney2.setText(String.valueOf(inpuStr_m2) + "円");
+
+        EditText workname3 = (EditText)root.findViewById(R.id.WorkName3);
+        String inpuStr_n3 = workname3.getText().toString();
+        EditText workmoney3 = (EditText)root.findViewById(R.id.WorkMoney3);
+        String inpuStr_m3 = workmoney3.getText().toString();
+        workmoney3.setText(String.valueOf(inpuStr_m3) + "円");
+
 
         /*詳細*/
         TextView day = (TextView)root.findViewById(R.id.Day);
@@ -68,6 +88,18 @@ public class SifutoFragment extends Fragment {
         time.setText("時間");
         TextView money = (TextView)root.findViewById(R.id.Money);
         money.setText("金額");
+
+        EditText day1 = (EditText)root.findViewById(R.id.Day1);
+        String inpuStr_day1 = day1.getText().toString();
+        EditText name1 = (EditText)root.findViewById(R.id.Name1);
+        String inpuStr_name1 = name1.getText().toString();
+        EditText time1 = (EditText)root.findViewById(R.id.Time1);
+        String inpuStr_time1 = time1.getText().toString();
+        workmoney1.setText(String.valueOf(inpuStr_time1) + "時間");
+        EditText money1 = (EditText)root.findViewById(R.id.Money1);
+        String inpuStr_money1 = money1.getText().toString();
+        workmoney1.setText(String.valueOf(inpuStr_money1) + "円");
+
 
         shiftViewModel = ViewModelProviders.of(this).get(ShiftViewModel.class);
         return root;
