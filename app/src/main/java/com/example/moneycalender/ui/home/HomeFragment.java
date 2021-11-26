5d365f2776ea60644b00698ea729a598b4778d10
s2071087 <s2071087@s.konan-u.ac.jp>
2021-11-26 15:26:27 +0900

    カレンダ＝を作成

--- app/src/main/java/com/example/moneycalender/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/moneycalender/ui/home/HomeFragment.java
@@ -1,10 +1,15 @@
 package com.example.moneycalender.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,20 +18,101 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.moneycalender.R;
-import com.example.moneycalender.ui.sifuto.SifutoViewModel;
+
+import java.text.BreakIterator;
 
 public class HomeFragment extends Fragment {
 
+    private int cnt = 0;
 
-    private SifutoViewModel sifutoViewModel;
+    private CalendarView calendar;
+
+    public HomeFragment() {
+        this.calendar = calendar;
+    }
+
+    public static HomeFragment newInstance(int count) {
+        HomeFragment homefragment = new HomeFragment();
+
+        Bandle args = new Bandle();
+        args.putInt("Counter",count);
+        homefragment.setArguments(args);
+
+        return homefragment;
+    }
+
+    public View onCreateView(@NonNull LayoutInflate inflater,
+                             ViewGroup container,Bundle savedInstanceState){
+        return inflater.inflate(R.layout.homefragment,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view,Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        Bundle args = getArguments();
+
+        if(args != null){
+            int count = args.getInt("Counter");
+            String str = "HomeFragment:"+String.valueOf(count);
+            cnt = count +1;
+
+            EditText eidtText = view.findViewById(R.id.editTextNumber);
+
+        }
+        EditText ediText = view.findViewById(R.id.editTextNumber2);
+        ediText = view.findViewById(R.id.editTextNumber3);
+    }
+
+    private void setArguments(Bandle args) {
+    }
+
+    @Override
+    public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.fragment_home);
+
+        CalenderView calender;
+        calender = findViewById(R.id.calendarView);
+        calendar.setOnDateChangeListener(
+                new OnDateChangeListener(){
+                    @Override
+                    public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+                    }
+
+
+                    public void onSelectDayChange(@NonNull CalendarView
+                                                  calendarView, int year, int month, int date){
+                        String message = year + "/"+(month +1)+"/"+date;
+                        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+
+                    }
+                }
+        );
+    }
+
+    private Context getApplicationContext() {
+        return null;
+    }
+
+    private CalenderView findViewById(int calendarView) {
+        return null;
+    }
+
+
+    private void setContentView(int fragment_home) {
+    }
+
+    private HomeViewModel homeViewModel;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-        sifutoViewModel =
-                ViewModelProviders.of(this).get(SifutoViewModel.class);
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        sifutoViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
@@ -34,5 +120,4 @@ public class HomeFragment extends Fragment {
         });
         return root;
     }
-
 }
